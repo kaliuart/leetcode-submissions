54ae8635696c59b026fb86547663ec95df87951c
kaliuart <kaliuzhnyiartur123@gmail.com>
2026-07-22 21:32:12 +0200

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,39 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = dict()
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in d:
+                return [
+                    d[diff], i
+                ]
+            else:
+                d[nums[i]] = i
+
+"""
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for (i, num in enumerate(nums)):
+            a.append([num, i])
+
+        a.sort()
+        i = 0
+        j = len(a) - 1
+
+        while i < j:
+            curr = a[i][0] + a[j][0]
+            
+            if curr == target:
+                return [
+                min(a[i][1], a[j][1]),
+                max(a[i][1], a[j][1])
+                
+                ]
+            
+            elif curr < target:
+                i++
+            
+            else:
+                j--
+        return []
+"""
+            
